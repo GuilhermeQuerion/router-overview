e269f57052248827619b3c4e9ef1a7ab8f3c68b5
GuilhermeQuerion <guilhermequerion@icloud.com>
2022-04-27 14:59:07 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Router Overview</h1>
+  <div class="router">
+    <router-link to="/pageA">Page A</router-link>
+    <router-link to="/pageB">Page B</router-link>
+    <router-link to="/pageC">Page C</router-link>
+  </div>
+  <h2>Go to Page Push</h2>
+  <div class="router">
+    <button @click="goToPage('/pageA')">Go to Page A</button>
+    <button @click="goToPage('/pageB')">Go to Page B</button>
+    <button @click="goToPage('/pageC')">Go to Page C</button>
+  </div>
+  <h2>Go to Page Replace</h2>
+  <div class="router">
+    <button @click="replacePage('/pageA')">Go to Page A</button>
+    <button @click="replacePage('/pageB')">Go to Page B</button>
+    <button @click="replacePage('/pageC')">Go to Page C</button>
+  </div>
+  <h2>Go to User (Parametros na url)</h2>
+  <div class="router">
+    <button @click="goToUser">Go to User</button>
+  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  methods: {
+    //Empilha os Históricos
+    goToPage(route) {
+      this.$router.push(route);
+    },
+    //Não permite o usuário voltar
+    replacePage(route) {
+      this.$router.replace(route);
+    },
+    goToUser(){
+      this.$router.push({name: "usuariovue", params:{userId: 2}})
+    }
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.router {
+  display: flex;
+  justify-content: space-around;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import {createRouter, createWebHistory} from 'vue-router';
+import PageA from "./components/PageA";
+import PageB from "./components/PageB";
+import PageC from "./components/PageC";
+import UsuarioVue from "./components/UsuarioVue";
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history:createWebHistory(),
+    routes: [
+        {path: "/pageA", component:PageA},
+        {path: "/pageB", component:PageB},
+        {path: "/pageC", component:PageC},
+        //Parametros na url
+        {path: "/user/:userId", name: "usuariovue" ,component:UsuarioVue}
+
+    ]
+});
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
